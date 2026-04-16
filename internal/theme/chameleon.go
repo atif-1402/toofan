b64fe1ff8bd6a27bf547de293fe13d28820b7b87
anom538 <whatech9@gmail.com>
2026-04-16 22:35:09 +0530

    Add files via upload

--- internal/theme/chameleon.go
+++ internal/theme/chameleon.go
@@ -5,8 +5,8 @@ import "github.com/charmbracelet/lipgloss"
 var Chameleon = Palette{
 	Name:       "chameleon",
 	Background: lipgloss.Color("0"),
-	Foreground: lipgloss.Color("7"),
-	Typed:      lipgloss.Color("15"),
+	Foreground: lipgloss.Color("8"),
+	Typed:      lipgloss.Color("7"),
 	Error:      lipgloss.Color("1"),
 	Cursor:     lipgloss.Color("15"),
 	Accent:     lipgloss.Color("6"),
